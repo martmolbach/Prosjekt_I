bfd5f598176e64a71baa8f8a9d00740ded768610
EvenKrakeli <144995875+EvenKrakeli@users.noreply.github.com>
2023-11-16 10:34:35 +0100

    Added displaySettings h file

--- testfiles/speedometertest/speedometertest.ino
+++ testfiles/speedometertest/speedometertest.ino
@@ -6,78 +6,15 @@
 #include <Wire.h>
 #include "speedometerVariables.h"
 #include "speedometerFunctions.h"
+#include "displaySettings.h"
 
 Zumo32U4Encoders encoder;
 Zumo32U4Motors motors;
-Zumo32U4OLED display;
 
 void setup()
 {
     encoder.init();
-    display.init();
-    display.clear();
-    display.setLayout21x8();
-}
-
-void displayValues()
-{
-    display.gotoXY(0, 0);
-    display.print("Speed:");
-    display.gotoXY(0, 1);
-    display.print(cmPerSecond);
-    display.print("cm/s");
-    display.gotoXY(0, 6);
-    display.print("Distance:");
-    display.gotoXY(0, 7);
-    display.print(distanceTraveled); // 3.75 er dm til hjulet
-    display.print("m");
-    display.gotoXY(16, 0);
-    display.print("Money");
-    display.gotoXY(11, 1);
-    display.print("Used:");
-    display.gotoXY(17, 1);
-    display.print("4kr");
-    display.gotoXY(9, 2);
-    display.print("Earned:");
-    display.gotoXY(17, 2);
-    display.print("2kr");
-    display.gotoXY(10, 3);
-    display.print("Total:");
-    display.gotoXY(17, 3);
-    display.print("7kr");
-    display.gotoXY(16, 6);
-    display.print("Time:");
-    display.gotoXY(16, 7);
-    display.print(timePassedForValues);
-    display.print("sec");
-}
-
-void displayAverages()
-{
-
-    display.gotoXY(0, 0);
-    display.print("Average speed:");
-    display.gotoXY(0, 1);
-    display.print(averageSpeed); // averageSpeed
-    display.print("cm/s");
-
-    display.gotoXY(0, 3);
-    display.print("Highest speed:");
-    display.gotoXY(0, 4);
-    display.print(highestSpeed); // highestSpeed
-    display.print("cm/s");
-
-    display.gotoXY(0, 6);
-    display.print("Over 70% max:");
-    display.gotoXY(0, 7);
-    display.print(secondsOverPercentage); // secondsOverPercentage
-    display.print("sec");
-
-    display.gotoXY(16, 6);
-    display.print("Time: ");
-    display.gotoXY(16, 7);
-    display.print(timePassedForAverage);
-    display.print("sec");
+    setupDisplay();
 }
 
 void loop()
@@ -125,7 +62,7 @@ void loop()
     // Clearer Display for å vise average, etter 60 sek
     if (showingValues == true && currentMillis - previousMillis2 >= 60000)
     {
-        display.clear();
+        clearDisplay();
         showingValues = false;
         previousMillis2 = currentMillis;
     }
@@ -141,7 +78,7 @@ void loop()
         // Clearer Display for neste iterasjon, etter 3 sek
         if (showingValues == false && currentMillis - previousMillis2 >= 3000)
         {
-            display.clear();
+            clearDisplay();
             highestSpeed = 0;
             secondsOverPercentage = 0;
             averageSpeed = 0;
